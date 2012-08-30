a20ee50594286fc665ef47a3574fc7d8202b0b4c
Norm <santnorm@gmail.com>
2012-08-30 16:45:35 -0400

    First working version of the remote.

--- remote.js
+++ remote.js
@@ -1,6 +1,5 @@
 var net = require('net');
 var http = require('http');
-var localPipeConnected = false;
 /*
  *  REQUEST   [ external request ] -> [ http server ] -> [ socket ] -> [ local socket ] -> [ request to localhost ]
  *  RESPONSE  [ localhost response ] -> [ local socket ] -> [ socket ] -> [ http response ] 
@@ -9,48 +8,55 @@ var localPipeConnected = false;
 
 
 var localPipe = net.createServer(function(c) { //'connection' listener
-	console.log('localPipe connected');
-	localPipeConnected = true;
+	console.log('[localPipe] connected');
 	c.on('end', function() {
-		localPipeConnected = false;
-		console.log('localPipe disconnected');
+		console.log('[localPipe] disconnected');
+		external.close();
 	});
-	c.write('hello\r\n');
-	c.pipe(c);
+//	c.write('hello\r\n');
+//	c.pipe(c);
+
+	// Only create the http server if we have a local connection
+	var external = http.createServer(function (req, res) {
+		console.log('[external] incomming request');
+
+
+		// Setup the callback for data responses
+		c.on('data', function(data){
+			console.log('[localPipe] data');
+			console.log(data.toString());
+			data = JSON.parse(data.toString());
+			res.statusCode = data.statusCode;
+			res.write(data.body);
+			res.end();
+		});
+
+		var cerial = {
+			'headers' : req.headers,
+			'url' : req.url,
+			'method' : req.method,
+			'statusCode' : req.statusCode
+		};
+		console.log('[external] request');
+		console.log(cerial);
+
+		var buff = new Buffer(JSON.stringify(cerial));
+
+		// Make the request to the pipe
+		c.write(buff);	
+	});
+
+	external.listen(7777, function(){
+		console.log('[external] listen');
+	});
+
 });
 
 localPipe.listen(5555, function() { //'listening' listener
-	console.log('localPipe bound');
+	console.log('[localPipe] bound');
 });
 
 
 
 
-
-var external = http.createServer(function (req, res) {
-	console.log('in comming connection');
-	if(!localPipeConnected){
-		res.writeHead(404, {'Content-Type': 'text/plain'});
-		res.end('localPipe note connected');
-	}
-});
-
-/*
-external.on('connect', function(request, socket, head){
-});
-*/
-/*
-external.listen('request', function(request, response){
-	console.log('in comming request');
-	if(!localPipeConnected){
-		response.writeHead(404, 'localPipe not connected');
-		response.end();
-		return
-	}
-});
-*/
-external.listen(7777, function(){
-console.log('listen');
-
-});
 console.log('up');
