6a6295278c8a6b2069f51f14454d92ee166848b2
Norm <santnorm@gmail.com>
2012-09-04 18:36:12 -0400

    Switching to dnode - now with working requests for large chunked requests and the full range of request methods.

--- localhost.js
+++ localhost.js
@@ -1,5 +1,7 @@
 var net = require('net');
 var http = require('http');
+var dnode = require('dnode');
+
 
 var remoteParams = {
 	'host' : '127.0.0.1',
@@ -11,99 +13,36 @@ var localParams = {
 	'port' : 80
 };
 
-verbose = false;
-var debug = function(){
-	var args = Array.prototype.slice.call(arguments);  
-	console.log(args[0]);
-	if(verbose){
-		console.log(args.slice(1));
-	}
-}
-
 // Connect to the proxy
-var remotePipe = net.connect(remoteParams, function() {
-	debug('remotePipe connected');
-});
-remotePipe.setKeepAlive(true);
-remotePipe.on('data', function(data) {
-	data = data.toString();
-	debug('[remote] got request', data);
-	d = data.split("\r\n");
-
-	for(i=0; i<d.length; i++){
-		data = d[i];
-		if(data.length < 1){
-			continue;
-		}
-		try {
-			data = JSON.parse(data);
-		} catch(e){
-			debug('[remote] invalid request');
-			debug(data);
-			var resp = {
-				'statusCode' : 404,
-				'body' : 'remotePipe could not parse request'
-			};
-
-			debug('[remote] resp:', resp);
-			var buff = new Buffer(JSON.stringify(resp));
-			remotePipe.write(buff);
-			return;
-		}
-
-		data.port         = localParams.port;
-		data.host         = localParams.host;
-		data.path         = data.url;
+var d = dnode(function(remote, conn){ 
+	this.proxyRequest = function(req, res, body, connected, gotData, gotError, gotEnd){
+		var data = {
+			'host' : localParams.host,
+			'port' : localParams.port,
+			'path' : req.url,
+			'headers' : req.headers,
+			'method' : req.method
+		};
 		data.headers.host = localParams.host+':'+localParams.port;
 
-		debug('[remote] request: ' + data.url);
+		var handle = req.method+": "+data.headers.host+req.url;
 
-		// Make a http request to the local http server
-		debug('[remote] making request: '+data.headers.host+data.url, data);
 		var request = http.request(data, function(response){
-			var seq = this.seq;
+			console.log('poxy request completed: '+handle);
 			response.on('data', function(chunk){
-				debug('[local] chunk', chunk);
-				debug('[local] chunk string size:' + chunk.toString().length);
-				debug('[local] chunk buffer size:' + chunk.length);
-				var resp = {
-					'statusCode' : response.statusCode,
-					'body' : chunk.toString('base64'),
-					'headers' : response.headers,
-					'seq' : seq
-				};
-				var buff = new Buffer(JSON.stringify(resp));
-				var flushed = remotePipe.write(buff+"\r\n");
-				if(!flushed){
-					remotePipe.pause();
-				}
+				console.log('data['+chunk.length+'] '+handle);
+				gotData(chunk.toString('base64'));
 			});
-		});
-		request.seq = data.seq;
+			response.on('end', gotEnd);
 
-		request.on('error', function(e){
-			debug('[local] fucking error', e);
-			debug(e);
-			var resp = {
-				'statusCode' : 404,
-				'body' : 'remotePipe local request error',
-				'seq' : this.seq
-			};
-
-			debug('[local] error resp', resp);
-			var buff = new Buffer(JSON.stringify(resp));
-			remotePipe.write(buff);
+			connected(request, response);
 		});
 
+		request.on('error', gotError);
+		if(req.method != 'GET'){
+			request.write(body);
+		}
 		request.end();
-	}
+	};
 
-//	remotePipe.end();
-});
-remotePipe.on('end', function() {
-	debug('remotePipe disconnected');
-});
-remotePipe.on('drain', function() {
-	debug('remotePipe drain');
-	remotePipe.resume();
-});
+}).connect(remoteParams.host, remoteParams.port);
--- remote.js
+++ remote.js
@@ -1,117 +1,48 @@
 var net = require('net');
 var http = require('http');
+var dnode = require('dnode');
 
-var verbose = false;
-var seq = 0;
-var queue = new Array();
-var debug = function(){
-        var args = Array.prototype.slice.call(arguments);
-        console.log(args[0]);
-        if(verbose){
-                console.log(args.slice(1));
-        }
-}
-
-
-
-
-/*
- *  REQUEST   [ external request ] -> [ http server ] -> [ socket ] -> [ local socket ] -> [ request to localhost ]
- *  RESPONSE  [ localhost response ] -> [ local socket ] -> [ socket ] -> [ http response ] 
- */
-	
-
-
-var localPipe = net.createServer(function(c) { //'connection' listener
-	debug('[localPipe] connected');
-	c.setMaxListeners(0);
-	c.setKeepAlive(true);
-
-	c.once('drain', function(data) {
-		debug('[localPipe] drain - write buffer empty');
-		c.resume();
-	});
-
-	// Only create the http server if we have a local connection
-	var external = http.createServer(function (req, res) {
-		queue[seq] = {
-			'req' : req,
-			'res' : res
-		};
-
-		// Setup the callback for data responses
-		c.once('data', function(data){
-			debug('[localPipe] buffer size:  ' + data.length);
-			data = data.toString();
-			debug('[localPipe] data', data);
-			debug('[localPipe] bytesRead ' + c.bytesRead);
-			debug('[localPipe] data size' + data.length);
-			debug(data);
-			var d = data.split("\r\n"); 
-			for (i=0; i<d.length; i++) {
-				var data = d[i];
-				if(data.length < 1){
-					continue;
-				}
-				try {
-					data = JSON.parse(data);
-				} catch (e){
-					debug(e);
-		//			debug(data);
-					continue;
-				}
-
-				if(queue[data.seq] !== false){
-					var r = queue[data.seq].res;
-					r.writeHead(data.statusCode, data.headers);
-					// debug('[localPipe] body string size: '+ data.body.length);
-					// debug('[localPipe] body string: '+ data.body);
-					r.write(new Buffer(data.body, 'base64'));
-					r.end();
-					
-				}
-			}
-		});
-
-		c.once('end', function() {
-			debug('[localPipe] socket is ending');
-			external.close();
-		});
-
-
-		var cerial = {
-			'seq' : seq,
-			'headers' : req.headers,
-			'url' : req.url,
-			'method' : req.method,
-			'statusCode' : req.statusCode
-		};
-		debug('[external] request:' + req.url, cerial);
-		// debug(cerial);
-
-		// Make the request to the pipe
-		var buff = new Buffer(JSON.stringify(cerial));
-		var flushed = c.write(buff+"\r\n");
-		if(!flushed){
-			c.pause();
-		}
-		seq++;	
-	});
-
-	external.setMaxListeners(0);
-
-	external.listen(7777, function(){
-		debug('[external] listen');
-	});
-
+// WEB SERVER
+var external = http.createServer();
+external.setMaxListeners(0);
+external.listen(7777, function(){
+	console.log('[external] listen');
 });
 
+// DNODE
+var server = dnode(function(remote, conn){
+	conn.on('ready', function() {
+		console.log('client ready');
+		external.on('request', function (req, res) {
+			var body = '';
 
-localPipe.listen(7123, function() { //'listening' listener
-	debug('[localPipe] bound');
-});
+			req.on('data', function(chunk){
+				body += chunk;
+			});
+			
+			req.on('end', function(){
+				console.log('http request ['+req.method+'] '+req.url);
+				console.log('end');
+				var connected = function(localReq, localRes){
+					res.writeHead(localRes.statusCode, localRes.headers);
+				};
 
+				var data = function(chunk){
+					console.log('data ['+chunk.length+'] '+req.url);
+					res.write(new Buffer(chunk, 'base64'));
+				};
 
+				var error = function(){
+					console.log('got error:' + req.url);
+					res.end();
+				};
 
+				var end = function(){
+					res.end();
+				};
 
-debug('up');
+				remote.proxyRequest(req, res, body, connected, data, error, end);
+			});
+		});
+	});
+}).listen(7123);
